d49383913fc1b3e7695522b86d0f515dbbfc51d6
singhabhishek22957 <singhabhishek22957@gmai.com>
2024-10-25 22:19:53 +0530

    Wroking on use middlewares error class in js and express in utils folder

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 // require('dotenv').config({path:'./env'})
-import dotenv from 'dotenv'
-
-import mongoose from "mongoose";
-import express from 'express';
-// import { DB_NAME } from "./constants.js";
+import dotenv from 'dotenv';
 import connectDB from "./db/index.js";
-const app = express();
+import { app } from './app.js';
+
 
 dotenv.config({
   path:'./env'
@@ -13,7 +10,21 @@ dotenv.config({
 
 
 
-connectDB();
+connectDB()
+.then(()=>{
+  app.on("error",(error)=>{
+    console.log("Error",error);
+    throw error
+    
+  })
+  app.listen(process.env.PORT||8000,()=>{
+    console.log(`server is running on port ${process.env.PORT}`)
+  })
+})
+.catch((error)=>{
+  console.log("Mongo Db connection failed");
+  
+})
 
 
 
