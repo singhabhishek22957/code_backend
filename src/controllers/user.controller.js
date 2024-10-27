a4c1651c909e231458403710cb1237ac16258665
singhabhishek22957 <singhabhishek22957@gmai.com>
2024-10-27 09:46:42 +0530

    Postman, File Handle and debug coverImage

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -26,7 +26,7 @@ const  registerUser = asyncHandler(async (req,res)=>{
   // get user details form frontend 
 
   const {fullName , username, email ,password } = req.body
-  console.log("email: ", email);
+  // console.log("email: ", email);
 
   // validation --not empty 
 
@@ -53,10 +53,19 @@ const  registerUser = asyncHandler(async (req,res)=>{
     throw new ApiError(409,"User already exists")
   }
 
+  // console.log(req.files);
+  
+
   // check for Image , check for avatar 
 
   const avatarLocalPath = req.files?.avatar[0]?.path
-  const coverImageLocalPath =req.files?.coverImage[0]?.path
+  // const coverImageLocalPath =req.files?.coverImage[0]?.path
+
+  let coverImageLocalPath;
+  if(req.files&&Array.isArray(req.files.coverImage) && req.files.coverImage.length>0){
+    coverImageLocalPath = req.files.coverImage[0].path
+  }
+
 
   if(!avatarLocalPath){
     throw new ApiError(400,"Avatar file is required")
@@ -77,7 +86,7 @@ const  registerUser = asyncHandler(async (req,res)=>{
     const user = await User.create({
       fullName,
       avatar: avatar.url,
-      avatar: coverImage?.url||"",
+      coverImage: coverImage?.url||"",
       email,
       username:username,
       password
